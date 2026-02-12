7eca007bc678cab36dd87c02490c8ccdeab2d73e
程序猿法师 <5123589+pengbin119@user.noreply.gitee.com>
2026-02-12 14:40:33 +0800

    目录结构调整

--- secretary/views/app.py
+++ secretary/views/app.py
@@ -75,7 +75,7 @@ class WxampRequestView(View):
                     
                 def handle_wxmessage_async(message_content):
                     def handle_wxmessage():
-                        loop = asyncio.get_event_loop()
+                        loop = asyncio.new_event_loop()
                         asyncio.set_event_loop(loop)
                         try:
                             loop.run_in_executor(secretary_aget(user_id, message_content, reply_hook))
